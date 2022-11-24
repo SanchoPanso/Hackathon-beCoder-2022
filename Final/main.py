8725269b3bc74afc4da0313b7ce8798a6e857ffb
Andrey <33835790+KAD1605@users.noreply.github.com>
2022-11-24 23:31:57 +0300

    feat: add menu

--- Final/main.py
+++ Final/main.py
@@ -7,6 +7,7 @@ import sys
 import time
 # import json
 from git import Repo
+import find_bugmakers
 
 try:
     import ujson as json
@@ -35,14 +36,21 @@ def yellow_text(text: str) -> str:
     return '\033[33m' + text + '\033[0m'
 
 
+def red_text(text: str) -> str:
+    """
+    Return red text for console (using ANSI escape sequences)
+    :param text: text to color
+    :return: red text for console
+    """
+    return '\033[31m' + text + '\033[0m'
+
 def get_url_from_input() -> str:
     """
     Get url from user input and validate it
     :return: url
     """
     while True:
-        url = input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-                    "Введите ссылку: ")
+        url = input("Введите ссылку: ")
 
         # If url is not valid, then print warning and ask again
         if not validators.url(url):
@@ -68,18 +76,19 @@ def get_commits_from_repo(repo: Repo):
     :param repo: repository
     """
     commits = list(repo.iter_commits())
-
+    print(green_text("\nФормирование списка коммитов... "
+                     "\nПожалуйста, подождите, это может занять") + red_text(" продолжительное ") +
+          green_text("время, если коммитов больше тысячи...\n"))
     res = []
     for i, commit in enumerate(commits):
-        res.append({"timestamp": commit.committed_datetime.timestamp(), 
-                 "id": commit.hexsha,
-                 "author": commit.author.name, 
-                 "fix": "fix" in commit.message.lower(),
-                 "files": list(commit.stats.files.keys())
-                })
+        res.append({"timestamp": commit.committed_datetime.timestamp(),
+                    "id": commit.hexsha,
+                    "author": commit.author.name,
+                    "fix": "fix" in commit.message.lower(),
+                    "files": list(commit.stats.files.keys())
+                    })
 
     with open("commits.json", "w", encoding="UTF-8") as file:
-        
         json.dump(res, file)
 
         # file.write(json.dumps([{"timestamp": commit.committed_datetime.timestamp(), 
@@ -101,15 +110,9 @@ def get_commits_from_repo(repo: Repo):
         #         f"{commit.committed_date}, {commit.hexsha}, {commit.author}, {fix_flax}, {files_list}\n")
 
 
-# def find_fix_lines():
-#     with open("commits.txt", "r", encoding="UTF-8") as file:
-#         fix_lines = []
-#         for count, line in enumerate(file):
-#             if "True" in line:
-#                 fix_lines.append(count)
-#     return fix_lines
-#
-#
+# _______________________________________________________________________________________________________________________
+# Old code for getting commits from repository in text format
+
 # def find_who_made_mistake():
 #     with open("commits.txt", "r", encoding="UTF-8") as file:
 #         fix_line = []
@@ -128,6 +131,88 @@ def get_commits_from_repo(repo: Repo):
 #             print(file_list)
 #
 #     print(fix_line)
+# _______________________________________________________________________________________________________________________
+
+def check_if_repo_exists() -> bool:
+    """
+    Check if repository exists
+    :return: True if exists, False otherwise
+    """
+    return os.path.exists("./repo")
+
+
+def delete_repo_and_commits():
+    if os.path.exists("./repo"):
+        os.system("del /f /s /q repo >nul 2>&1")
+        shutil.rmtree("./repo")
+    if os.path.exists("./commits.txt"):
+        os.remove("./commits.txt")
+
+def menu():
+    if check_if_repo_exists():
+        common_error_arr, error_arr, numbers, freq, bugs, commits = find_bugmakers.find_errors_if_file()
+        print(green_text("\nРепозиторий уже скачан! Повторное скачивание не требуется."))
+    else:
+        common_error_arr, error_arr, numbers, freq, bugs, commits = [], [], [], [], [], []
+
+    while True:
+        print("\n")
+        print(green_text("1") + ". Скачать репозиторий и получить все коммиты в файл")
+        print(green_text("2") + ". Построить гистограмму для первой гипотезы")
+        print(green_text("3") + ". Построить гистограмму для второй гипотезы")
+        print(green_text("4") + ". Вывести вероятность ошибки в файле")
+        print(green_text("0") + ". Удалить все файлы репозитория и информацию о коммитах")
+        print(green_text("q") + ". Выход")
+        print("Репозиторий для тестов: https://github.com/usememos/memos")
+
+        choice = input("Введите номер пункта меню: ")
+        if choice == "1":
+            url = get_url_from_input()
+            delete_repo_and_commits()
+            get_repo_from_url(url)
+            get_commits_from_repo(Repo("./repo"))
+            common_error_arr, error_arr, numbers, freq, bugs, commits = find_bugmakers.find_errors_if_file()
+            print(green_text("Репозиторий скачан и все коммиты получены успешно!"))
+            continue
+
+        elif choice == "2":
+            if not check_if_repo_exists():
+                print(red_text("Репозиторий не найден!"))
+                continue
+            print(green_text("Построение гистограммы для первой гипотезы..."))
+            find_bugmakers.display_histogram_first(common_error_arr, error_arr)
+            continue
+
+        elif choice == "3":
+            if not check_if_repo_exists():
+                print(red_text("Репозиторий не найден!"))
+                continue
+            print(green_text("Построение гистограммы для второй гипотезы..."))
+            find_bugmakers.display_histogram_second(numbers, freq)
+            continue
+
+        elif choice == "4":
+            if not check_if_repo_exists():
+                print(red_text("Репозиторий не найден!"))
+                continue
+            print(green_text("Вывод вероятности ошибки в файле..."))
+            find_bugmakers.bug_prediction(numbers, freq, bugs, commits)
+            continue
+
+        elif choice == "0":
+            print("Удаление всех файлов...")
+            delete_repo_and_commits()
+            print(green_text("Удаление завершено!"))
+            continue
+
+        elif choice == "q":
+            print(green_text("Выход..."))
+            break
+
+        else:
+            print(yellow_text("Некорректный ввод!"))
+            continue
+
 
 def main():
     # if os.path.exists("./repo"):
@@ -135,15 +220,17 @@ def main():
     #     shutil.rmtree("./repo")
     # if os.path.exists("./commits.txt"):
     #     os.remove("./commits.txt")
-
+    #
     # url = get_url_from_input()
     # get_repo_from_url(url)
-
-    repo = Repo("./repo")
-    get_commits_from_repo(repo)
+    #
+    # repo = Repo("./repo")
+    # get_commits_from_repo(repo)
 
     # fix_lines = find_fix_lines()
     # find_who_made_mistake()
+
+    menu()
 
 
 if __name__ == '__main__':
