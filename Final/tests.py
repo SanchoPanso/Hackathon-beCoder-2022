d24cf5ade04a1ae7061ff697b5063e66dc8e4af2
Лазарев Максим <90333341+mmallumm@users.noreply.github.com>
2022-11-24 19:28:23 +0300

    add test for find_bug_frequency

--- Final/tests.py
+++ Final/tests.py
@@ -2,6 +2,7 @@ import pytest
 import json
 import os
 from find_bugmakers import find_bugs
+from find_bugmakers import find_bug_frequency
 
 def test_commit_1():
     with open("test_source\\test_1.json", 'r') as file:
@@ -18,7 +19,7 @@ def test_commit_2():
     bugs = find_bugs(commits)
     assert bugs == ground_truth
 
-def test_commit_3():
+def test_commit_3(): 
     with open("test_source\\test_3.json", 'r') as file:
         commits = json.load(file)
     ground_truth = [{'timestamp': 800, 'author': 'bugmaker_1', 'file': 'web/src/components/MemoResources.tsx'},
@@ -26,7 +27,17 @@ def test_commit_3():
     bugs = find_bugs(commits)
     assert bugs == ground_truth
 
+def test_commit_4():
+    with open("test_source\\test_4.json", 'r') as file:
+        commits = json.load(file)
+    bugs = find_bugs(commits)
+    new_list = find_bug_frequency(bugs)
+    ground_truth = {'bugmaker_1': {'web/src/components/MemoResources.tsx': 1},
+                     'bugmaker_2': {'web/src/less/memo-resources.less': 2}}
+    assert new_list == ground_truth
+
 if __name__ == '__main__':
     #test_commit_1()
     #test_commit_2()
-    test_commit_3()
+    #test_commit_3()
+    test_commit_4()
