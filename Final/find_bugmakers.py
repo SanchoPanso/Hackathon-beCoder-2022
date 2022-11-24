8a056daafb6c5b9a5f2233ee284bba204f7ae26f
Andrey <33835790+KAD1605@users.noreply.github.com>
2022-11-24 23:32:39 +0300

    refactor: for menu

--- Final/find_bugmakers.py
+++ Final/find_bugmakers.py
@@ -30,7 +30,6 @@ def find_bugs(commit_table: list) -> list:
                 for i in range(idx + 1, len(commit_table)):
                     old_commit = commit_table[i]
                     if fixed_file in old_commit["files"]:
-                        
                         bugs.append(
                             {
                                 "timestamp": old_commit['timestamp'],
@@ -40,11 +39,11 @@ def find_bugs(commit_table: list) -> list:
                         )
 
                         break
-    
+
     return bugs
 
+
 def find_bug_frequency(bugs: list) -> list:
-    
     bug_frequencies = {}
 
     for bug in bugs:
@@ -53,12 +52,12 @@ def find_bug_frequency(bugs: list) -> list:
 
         if author not in bug_frequencies:
             bug_frequencies[author] = {}
-        
+
         if file in bug_frequencies[author]:
             bug_frequencies[author][file] += 1
-        else: 
+        else:
             bug_frequencies[author][file] = 1
-    
+
     return bug_frequencies
 
 
@@ -71,61 +70,60 @@ def find_commits_numbers(commits: list):
 
         if author not in commits_numbers:
             commits_numbers[author] = {}
-        
+
         for file in files:
             if file in commits_numbers[author]:
                 commits_numbers[author][file] += 1
-            else: 
+            else:
                 commits_numbers[author][file] = 1
 
-    
     return commits_numbers
 
 
-def find_statistics1(numbers: dict, bug_freq: dict):
+def find_statistics1(numbers: dict, bug_freq: list):
     statistic = {}
 
     for author in numbers:
-        
+
         if author not in statistic:
             statistic[author] = {}
-        
+
         for file in numbers[author]:
             numbers_of_commits = numbers[author][file]
 
-            if author in bug_freq and file in bug_freq[author]: 
+            if author in bug_freq and file in bug_freq[author]:
                 numbers_of_bugs = bug_freq[author][file]
             else:
                 numbers_of_bugs = 0
-        
+
             statistic[author][file] = numbers_of_bugs / numbers_of_commits
-    
+
     return statistic
 
 
-def find_statistics2(numbers: dict, bug_freq: dict):
+def find_statistics2(__numbers: dict, bug_freq: list):
     statistic = {}
 
-    for author in numbers:
-        
+    for author in __numbers:
+
         if author not in statistic:
             statistic[author] = {'x': [], 'y': []}
-        
-        for file in numbers[author]:
-            numbers_of_commits = numbers[author][file]
 
-            if author in bug_freq and file in bug_freq[author]: 
+        for file in __numbers[author]:
+            numbers_of_commits = __numbers[author][file]
+
+            if author in bug_freq and file in bug_freq[author]:
                 numbers_of_bugs = bug_freq[author][file]
             else:
                 numbers_of_bugs = 0
-        
+
             statistic[author]['y'].append(numbers_of_commits)
             statistic[author]['x'].append(numbers_of_bugs / numbers_of_commits)
-    
+
     return statistic
 
 
-def prepare_troublefiles_data(numbers: dict, bug_freq: dict, bugs: list, commits: list):
+def prepare_troublefiles_data(numbers: dict, bug_freq: list, bugs: list, commits: list):
     x, y = [], []
     statistic = find_statistics1(numbers, bug_freq)
     bug_timestamps = [bug['timestamp'] for bug in bugs]
@@ -140,17 +138,17 @@ def prepare_troublefiles_data(numbers: dict, bug_freq: dict, bugs: list, commits
 
             if file in statistic[author]:
                 bug_probs += statistic[author][file]
-        
+
         x.append([bug_probs])
         y.append([1] if timestamp in bug_timestamps else [0])
 
-        
-        
     return x, y
 
 
 def otsu(x, y):
     total = sum(y)
+    if total == 0:
+        return 0
     sum_y = sum([i * j for i, j in zip(x, y)])
     sum_y2 = sum([i * i * j for i, j in zip(x, y)])
     max_x = max(x)
@@ -166,71 +164,78 @@ def get_hist(x, num=10):
     hist = [0 for i in range(num)]
     for xi in x:
         for i in range(num):
-            if i * 1/num <= xi <= (i + 1) * 1/num:
+            if i * 1 / num <= xi <= (i + 1) * 1 / num:
                 hist[i] += 1
 
-    return hist 
+    return hist
 
 
-if __name__ == '__main__':
-
+def find_errors_if_file():
     with open('./commits.json', 'r') as file:
-        commits = json.load(file)
-    
-    #print(commits)
+        __commits = json.load(file)
 
-    bugs = find_bugs(commits)
-    freq = find_bug_frequency(bugs)
+    # print(commits)
 
-    numbers = find_commits_numbers(commits)
+    __bugs = find_bugs(__commits)
+    bug_freq = find_bug_frequency(__bugs)
 
-    statistic = find_statistics1(numbers, freq)
-    #author = list(statistic.keys())[0]
+    comm_numbers = find_commits_numbers(__commits)
 
-    common_error_arr = []
-    for author in statistic.keys():
-        print(author)
-        #print(statistic[author])
+    statistic = find_statistics1(comm_numbers, bug_freq)
+    # author = list(statistic.keys())[0]
+    __common_error_arr = []
+    __error_arr = []
 
-        error_arr = [statistic[author][file] for file in statistic[author]]# if statistic[author][file] != 0]
-        #print(error_arr)
-        common_error_arr += error_arr
+    for authors in statistic.keys():
+        # print(authors)
+        __error_arr = [statistic[authors][file] for file in statistic[authors]]  # if statistic[authors][file] != 0]
+        __common_error_arr += __error_arr
 
-    
-    pd.Series(common_error_arr).plot(kind='hist', title = 'height', bins=5)
+    return __common_error_arr, __error_arr, comm_numbers, bug_freq, __bugs, __commits
+
+
+def display_histogram_first(__common_error_arr: list, __error_arr: list):
+    plt.rcParams.update({'font.size': 14})
+
+    pd.Series(__common_error_arr).plot(kind='hist', title='height', bins=5)
     plt.xlim([0, 1.2])
-    
 
-    x = [0.1 * i for i in range(10)]
-    y = get_hist(error_arr)
-    print(otsu(x, y))
-    # plt.axvline(x = otsu(x, y), color = 'b', label = 'axvline - full height')
+    __x = [0.1 * __i for __i in range(10)]
+    __y = get_hist(__error_arr)
+    print("Порог Оцу: ", otsu(__x, __y))
+
+    plt.title('Histogram of errors (Hypothesis 1)')
+    plt.xlabel('Error')
+    plt.ylabel('Number of files')
+
+    plt.axvline(np.mean(__error_arr), color='b', linestyle='solid', linewidth=2)
 
     plt.show()
 
 
+def display_histogram_second(__numbers: dict, __freq: list):
+    plt.rcParams.update({'font.size': 14})
 
     common_x2, common_y2 = [], []
-    statistic2 = find_statistics2(numbers, freq)
+    statistic2 = find_statistics2(__numbers, __freq)
     reg = LinearRegression()
     for author in statistic2.keys():
         x2 = statistic2[author]['x']
         y2 = statistic2[author]['y']
 
-
         x2_tmp, y2_tmp = [], []
-        for i in range(len(x2)):
-            if x2[i] != 0:
-                x2_tmp.append(x2[i])
-                y2_tmp.append(y2[i])
+        for __i in range(len(x2)):
+            if x2[__i] != 0:
+                x2_tmp.append(x2[__i])
+                y2_tmp.append(y2[__i])
         x2, y2 = x2_tmp, y2_tmp
-        if len(x2) == 0: x2 = [0]
-        if len(y2) == 0: y2 = [0]
+        if len(x2) == 0:
+            x2 = [0]
+        if len(y2) == 0:
+            y2 = [0]
 
         common_x2 += x2
         common_y2 += y2
-
-
 
     reg.fit(np.array([common_x2]).T, np.array([common_y2]).T)
 
@@ -238,18 +243,25 @@ if __name__ == '__main__':
 
     line_x = np.arange(0, 1, 0.1)
     line_y = reg.predict(line_x.reshape(-1, 1))
-    print(line_x)
-    print(line_y)
+    # print(line_x)
+    # print(line_y)
     plt.plot(line_x, line_y, color='orange')
+
+    plt.title('Histogram of errors (Hypothesis 2)')
+    plt.xlabel('Error')
+    plt.ylabel('Number of times working in this file')
 
     plt.xlim([0, 1.2])
     plt.show()
 
 
-    x, y = prepare_troublefiles_data(numbers, freq, bugs, commits)
-    print(len(x))
+def bug_prediction(__numbers: dict, __freq: list, __bugs: list, __commits: list):
+    x, y = prepare_troublefiles_data(__numbers, __freq, __bugs, __commits)
+
+    # print(len(x))
 
     x0, y0, x1, y1 = [], [], [], []
+
     for i in range(len(x)):
         if y[i][0] == 0:
             x0.append(x[i])
@@ -257,38 +269,40 @@ if __name__ == '__main__':
         else:
             x1.append(x[i])
             y1.append(y[i])
-    print('x0', len(x0))
-    print('x1', len(x1))
+
+    # print('x0', len(x0))
+    # print('x1', len(x1))
+
     x1 = x1 * (len(x0) // len(x1))
     y1 = y1 * (len(y0) // len(y1))
 
     x = x0 + x1
     y = y0 + y1
 
-    train_x, test_x, train_y, test_y = train_test_split(x, y, test_size = 0.3, random_state = 97)
-
+    #
+    train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.3, random_state=97)
 
     # train_x, train_y = x[:int(0.8*len(x))], y[:int(0.8*len(y))]
     # test_x, test_y = x[int(0.8*len(x)):], y[int(0.8*len(y)):]
+
     model = LogisticRegression()
     model.fit(train_x, train_y)
-    print(model.score(test_x, test_y))
+
+    # print(model.score(test_x, test_y))
+
     test_pred_y = model.predict(test_x)
-    print(test_pred_y)
+    # print(test_pred_y)
+
     print('accuracy:', accuracy_score(test_y, test_pred_y))
     print('precision:', precision_score(test_y, test_pred_y))
     print('recall:', recall_score(test_y, test_pred_y))
 
 
+if __name__ == '__main__':
+    common_error_arr, error_arr, numbers, freq, bugs, commits = find_errors_if_file()
 
+    display_histogram_first(common_error_arr, error_arr)
 
+    display_histogram_second(numbers, freq)
 
-
-
-
-
-
-
-
-
-
+    bug_prediction(numbers, freq, bugs, commits)
